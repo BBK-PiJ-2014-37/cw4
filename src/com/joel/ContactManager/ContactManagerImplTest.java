7519777c6242d7c1481d287df77cf8e6892e1d7f
Joel Armando <joelarma@gmail.com>
2014-12-14 20:02:48 +0000

    Add tests for future meetings

--- src/com/joel/ContactManager/ContactManagerImplTest.java
+++ src/com/joel/ContactManager/ContactManagerImplTest.java
@@ -2,7 +2,12 @@ package com.joel.ContactManager;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.Before;
@@ -92,4 +97,126 @@ public class ContactManagerImplTest {
 		assertEquals("Wrong subsets selected", aSet, rSet);
 	}
 
+	private void setUpContacts() {
+		mgr.addNewContact("Moe Stooge", "The smart one");
+		mgr.addNewContact("Larry Stooge", "The crazy one");
+		mgr.addNewContact("Curly Stooge", "The bald one");
+		moe = mgr.getContacts("Moe").iterator().next();
+		larry = mgr.getContacts("Larry").iterator().next();
+		curly = mgr.getContacts("Curly").iterator().next();
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void futureMeetingInPastTest() {
+		setUpContacts();
+		mgr.addFutureMeeting(
+				mgr.getContacts(moe.getId()),
+				new GregorianCalendar(1963, 01, 30));
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void futureMeetingEmptyGuests() {
+		setUpContacts();
+		mgr.addFutureMeeting(
+				new HashSet<Contact>(),
+				new GregorianCalendar(2020, 01, 30));
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void futureMeetingUnknownGuest() {
+		setUpContacts();
+		Contact wrongContact = new ContactImpl("Charlie Chaplin", "The Tramp");
+		Set<Contact> guests = new HashSet<Contact>();
+		guests.add(moe);
+		guests.add(wrongContact);
+		mgr.addFutureMeeting(
+				guests,	new GregorianCalendar(2020, 01, 30));
+	}
+
+	@Test
+	public void getFutureMeetingWhileEmpty() {
+		assertNull("No meeting expected", mgr.getMeeting(1));
+	}
+	
+	@Test
+	public void addFutureMeetingTest() {
+		setUpContacts();
+		Calendar date = new GregorianCalendar(2020, 01, 30);
+		Set<Contact> guests = mgr.getContacts(moe.getId());
+		int meetId = mgr.addFutureMeeting(guests, date);
+		FutureMeeting meet = mgr.getFutureMeeting(meetId);
+		assertNotNull("Expected a meeting", meet);
+		assertEquals("Wrong guest list", guests, meet.getContacts());
+		assertEquals("Wrong date", date, meet.getDate());
+		assertEquals("Expected to find", meet, mgr.getMeeting(meetId));
+	}
+
+	@Test
+	public void addSeveralFutureMeetingsTest() {
+		setUpContacts();
+		
+		Calendar date = new GregorianCalendar(2020, 01, 30);
+		Calendar otherDate = new GregorianCalendar(2020, 02, 12);
+		Set<Contact> guests1 = mgr.getContacts(moe.getId());
+		Set<Contact> guests2 = mgr.getContacts(moe.getId(), larry.getId());
+		Set<Contact> guests3 = mgr.getContacts(moe.getId());
+
+		int meet1Id = mgr.addFutureMeeting(guests1, date);
+		int meet2Id = mgr.addFutureMeeting(guests2, date);
+		int meet3Id = mgr.addFutureMeeting(guests3, otherDate);
+	
+		FutureMeeting meet1 = mgr.getFutureMeeting(meet1Id);
+		assertNotNull("Expected meeting", meet1);
+		assertEquals("Wrong guest list", guests1, meet1.getContacts());
+		assertEquals("Wrong date list", date, meet1.getDate());
+		FutureMeeting meet2 = mgr.getFutureMeeting(meet2Id);
+		assertNotNull("Expected meeting", meet2);
+		assertEquals("Wrong guest list", guests2, meet2.getContacts());
+		assertEquals("Wrong date list", date, meet2.getDate());
+		FutureMeeting meet3 = mgr.getFutureMeeting(meet3Id);
+		assertNotNull("Expected meeting", meet3);
+		assertEquals("Wrong guest list", guests3, meet3.getContacts());
+		assertEquals("Wrong date list", otherDate, meet3.getDate());
+
+		assertEquals("Find mismatch",
+				mgr.getFutureMeeting(meet1Id), mgr.getMeeting(meet1Id));
+		assertEquals("Find mismatch",
+				mgr.getFutureMeeting(meet2Id), mgr.getMeeting(meet2Id));
+		assertEquals("Find mismatch",
+				mgr.getFutureMeeting(meet3Id), mgr.getMeeting(meet3Id));
+		
+		assertNull("Should not be in the past", mgr.getPastMeeting(meet1Id));
+		assertNull("Should not be in the past", mgr.getPastMeeting(meet2Id));
+		assertNull("Should not be in the past", mgr.getPastMeeting(meet3Id));
+
+		Meeting[] expectedMeetsMoe = {meet1, meet2, meet3};
+		assertEquals("Wrong meeting list",
+				new HashSet<Meeting>(Arrays.asList(expectedMeetsMoe)),
+				new HashSet<Meeting>(mgr.getFutureMeetingList(moe)));
+		Meeting[] expectedMeetsLarry = {meet2};
+		assertEquals("Wrong meeting list",
+				new HashSet<Meeting>(Arrays.asList(expectedMeetsLarry)),
+				new HashSet<Meeting>(mgr.getFutureMeetingList(larry)));
+		assertEquals("Wrong meeting list",
+				new HashSet<Meeting>(),
+				new HashSet<Meeting>(mgr.getFutureMeetingList(curly)));
+
+		Meeting[] expectedMeetsDate = {meet1, meet2};
+		assertEquals("Wrong meeting list",
+				new HashSet<Meeting>(Arrays.asList(expectedMeetsDate)),
+				new HashSet<Meeting>(mgr.getFutureMeetingList(date)));
+		Meeting[] expectedMeetsOtherDate = {meet3};
+		assertEquals("Wrong meeting list",
+				new HashSet<Meeting>(Arrays.asList(expectedMeetsOtherDate)),
+				new HashSet<Meeting>(mgr.getFutureMeetingList(otherDate)));
+		assertEquals("Wrong meeting list",
+				new HashSet<Meeting>(),
+				new HashSet<Meeting>(mgr.getFutureMeetingList(
+						new GregorianCalendar(2020, 10, 11))));
+
+		assertEquals("Wrong meeting list",
+				new HashSet<PastMeeting>(),
+				new HashSet<PastMeeting>(mgr.getPastMeetingList(curly)));
+
+	}
 }
