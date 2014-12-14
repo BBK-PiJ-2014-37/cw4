7a44026709201b85264cf7eb12aae9fb11e66a84
Joel Armando <joelarma@gmail.com>
2014-12-14 18:32:53 +0000

    Clean up and don use toArray()

--- src/com/joel/ContactManager/ContactManagerImplTest.java
+++ src/com/joel/ContactManager/ContactManagerImplTest.java
@@ -5,51 +5,54 @@ import static org.junit.Assert.*;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ContactManagerImplTest {
+	ContactManager mgr;
+	Contact moe, larry, curly;
+	
+	@Before
+	public void setUp() {
+		mgr = new ContactManagerImpl();
+	}
 	
 	@Test(expected=NullPointerException.class)
 	public void nullContactNamePatternTest() {
-		ContactManager mgr = new ContactManagerImpl();
 		mgr.getContacts((String)null);
 	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void unknownContactIdTest() {
-		ContactManager mgr = new ContactManagerImpl();
 		mgr.getContacts(0);
 	}
 
 	@Test
 	public void emptyContactListTest() {
-		ContactManager mgr = new ContactManagerImpl();
 		Set<Contact> contacts = mgr.getContacts("");
 		assertEquals("Expected empty list", 0, contacts.size());
 	}
 
 	@Test
 	public void addOneContactTest() {
-		ContactManager mgr = new ContactManagerImpl();
 		mgr.addNewContact("Moe Stooge", "The smart one");
 		Set<Contact> contacts = mgr.getContacts("");
 		assertEquals("Wrong number of contacts", 1, contacts.size());
-		Contact[] contactArray = contacts.toArray(new Contact[0]);
-		assertEquals("Wrong name set", "Moe Stooge",
-				contactArray[0].getName());	
-		assertEquals("Wrong notes set", "The smart one",
-				contactArray[0].getNotes());	
+		Contact c = contacts.iterator().next();
+		assertEquals("Wrong name set", "Moe Stooge", c.getName());	
+		assertEquals("Wrong notes set", "The smart one", c.getNotes());	
 	}
 
 	@Test
 	public void addAndFindContactTest() {
-		ContactManager mgr = new ContactManagerImpl();
 		mgr.addNewContact("Moe Stooge", "The smart one");
 		mgr.addNewContact("Larry Stooge", "The crazy one");
 		mgr.addNewContact("Curly Stooge", "The bald one");
 		Set<Contact> contacts = mgr.getContacts("");
 		assertEquals("Wrong number of contacts", 3, contacts.size());
-		Contact moe = null, larry = null, curly = null;
+		moe = null;
+		larry = null;
+		curly = null;
 		Iterator<Contact> i = contacts.iterator();
 		while (i.hasNext()) {
 			Contact c = (Contact)i.next();
@@ -75,13 +78,13 @@ public class ContactManagerImplTest {
 		assertEquals("Notes mismatch", "The bald one", curly.getNotes());
 		Set<Contact> aSet = mgr.getContacts("Moe");
 		assertEquals("Wrong number of contacts found", 1, aSet.size());
-		assertEquals("Wrong contact found", moe, aSet.toArray()[0]);
+		assertEquals("Wrong contact found", moe, aSet.iterator().next());
 		aSet = mgr.getContacts("Larry");
 		assertEquals("Wrong number of contacts found", 1, aSet.size());
-		assertEquals("Wrong contact found", larry, aSet.toArray()[0]);
+		assertEquals("Wrong contact found", larry, aSet.iterator().next());
 		aSet = mgr.getContacts("Curly");
 		assertEquals("Wrong number of contacts found", 1, aSet.size());
-		assertEquals("Wrong contact found", curly, aSet.toArray()[0]);
+		assertEquals("Wrong contact found", curly, aSet.iterator().next());
 		aSet = mgr.getContacts("Stooge");
 		assertEquals("Wrong set returned", contacts, aSet);
 		Set<Contact> rSet =  mgr.getContacts("r");
